8d83630ef02ae56ed096e44b69b0212e6a0d8c58
LeeMyungSeob <66297203+LeeMyungSeob@users.noreply.github.com>
2020-06-12 22:35:00 +0900

    Update 이미지 전처리.py

--- 이미지 전처리.py
+++ 이미지 전처리.py
@@ -12,25 +12,23 @@ import os
 import shutil
 from keras.preprocessing import image
 
-
+name = input("전처리할 사진의 이름을 입력하세요: ")
 img_dir = r'C:\Users\myung\AI\images'
-fnames = ['imgDog{}.jpg'.format(i) for i in range(10)]
+for i in range(1,11):
+ img_name = '{}{}.jpg'.format(name,i) #for x in range(10) .format(i)
+ img_path = os.path.join(img_dir, img_name)
 
-img_name = 'imgDog6.jpg' #for x in range(10) .format(i)
-img_path = os.path.join(img_dir, img_name)
+ img = image.load_img(img_path, target_size=(250, 250))
+ img_tensor = image.img_to_array(img)
 
-img = image.load_img(img_path, target_size=(250, 250))
-img_tensor = image.img_to_array(img)
-
-img_tensor = np.expand_dims(img_tensor, axis=0)
-img_tensor.shape
-(1, 250, 250, 3)
+ img_tensor = np.expand_dims(img_tensor, axis=0)
+ img_tensor.shape
+ (1, 250, 250, 3)
  
  # scaling into [0, 1]
-img_tensor /= 255
+ img_tensor /= 255
 
-plt.rcParams['figure.figsize'] = (10, 10)
-plt.imshow(img_tensor[0])
-plt.show()
-print(img_tensor[0])
+ plt.rcParams['figure.figsize'] = (10, 10)
+ plt.imshow(img_tensor[0])
+ plt.show()
 
